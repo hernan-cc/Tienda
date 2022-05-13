ee7da17dfcb609f011fee37db35822f5d94e8ca3
hidden <hernancaravario@hotmail.com>
2022-05-13 18:35:40 -0300

    cleanup

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,22 +1,6 @@
 import React,{Component} from "react"
 import Pic from "./Pic.jsx"
 
-// class Productos extends Component {
-//     render(){
-//         return(
-//             <div>
-//                 <header>Productos</header>
-//                 <p>Logitech MX Master 3</p>
-//                 <div><Img /></div>
-//                 <p>Mouse inalambrico multidispositivo de alta productividad</p>
-//                 <p>$ 10.000</p>
-//                 <p>SKU: 5209852ASAS309</p>
-//                 <p>Disponible: 10</p>
-//             </div>
-//         )
-//     }
-// }
-
 let infoProd = ['nombre', 'img', 'descripcion', 'precio', 'sku', 'stock']
 
 class Productos extends Component {
@@ -31,8 +15,8 @@ class Productos extends Component {
                 <Pic fuente = {this.props.img}/>
                 <div className="descripcion">{this.props.descripcion}</div>
                 <div className="precio">{this.props.precio}</div>
-                <div className="sku">{this.props.SKU}</div>
-                <div className="stock">{this.props.stock}</div>
+                <div className="sku">SKU: {this.props.SKU}</div>
+                <div className="stock">Stock: {this.props.stock}</div>
             </div>
         )
     }
