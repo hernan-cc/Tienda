33bf699f2bde542fc6f79515b8fcbb9bb3e70217
hidden <hernancaravario@hotmail.com>
2022-05-13 19:03:54 -0300

    cleanup

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,22 +3,6 @@ import Productos from './Productos.jsx'
 import mxM3 from './mxM3.jpg'
 import mxA3 from './mxA3.jpeg'
 
-// const prod1 = <Productos
-//     nombre='Logitech MX Master 3'
-//     descripcion='Mouse inalambrico multidispositivo de alta productividad'
-//     precio='$ 10.000'
-//     SKU='5209852ASAS309'
-//     stock='10'
-//     />
-
-// const prod2 = <Productos
-//     nombre='MX Anywhere 3'
-//     descripcion='Mouse inalambrico multidispositivo de alta portabilidad'
-//     precio='$ 9.000'
-//     SKU='653574UIYLK978'
-//     stock='15'
-//     />
-
 class Home extends Component{
     render(){
         return(
